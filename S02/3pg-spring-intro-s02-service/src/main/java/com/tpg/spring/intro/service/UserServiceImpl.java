46d057e17579512ab3a53e860b5331ff7acae133
Bogdan Solga <bogdan.solga@gmail.com>
2014-06-30 16:20:22 +0300

    Second session - first draft

---
+++ S02/3pg-spring-intro-s02-service/src/main/java/com/tpg/spring/intro/service/UserServiceImpl.java
@@ -0,0 +1,25 @@
+package com.tpg.spring.intro.service;
+
+import com.tpg.spring.intro.dao.UserDAO;
+import com.tpg.spring.intro.entities.User;
+
+/**
+ * UserServiceImpl class
+ *
+ * @author bogdan.solga
+ *
+ * Date: 27.06.2014, time: 09:36
+ */
+public class UserServiceImpl implements UserService {
+
+    private UserDAO usersDAO;
+
+    public UserServiceImpl(UserDAO usersDAO) {
+        this.usersDAO = usersDAO;
+    }
+
+    @Override
+    public User getJohnDoe() {
+        return usersDAO.getJohnDoe();
+    }
+}
