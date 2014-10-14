e52ea1a783b7c041c49a76e8741fae8ee351d7d1
Bogdan Solga <bogdan.solga@gmail.com>
2014-10-14 15:52:44 +0300

    Added @Configuration based config and usage

---
+++ S02/3pg-spring-intro-s02-service/src/test/java/com/tpg/spring/intro/annotation/ApplicationConfig.java
@@ -0,0 +1,22 @@
+package com.tpg.spring.intro.annotation;
+
+import com.tpg.spring.intro.dao.UserDAO;
+import com.tpg.spring.intro.dao.UserDAOImpl;
+import com.tpg.spring.intro.service.UserService;
+import com.tpg.spring.intro.service.UserServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApplicationConfig {
+
+    @Bean
+    public UserDAO getUserDAO() {
+        return new UserDAOImpl();
+    }
+
+    @Bean
+    public UserService getUserService() {
+        return new UserServiceImpl(getUserDAO());
+    }
+}
