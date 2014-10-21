2b90b802d6f3feff0b78622a647b0b420cdd862d
Bogdan Solga <bogdan.solga@gmail.com>
2014-10-21 11:55:09 +0300

    Cleaned-up the unneeded dependencies and repos, added the missing <mvc:annotation-driven> tag

--- S03/3pg-spring-intro-s03-data-access/src/main/java/com/tpg/spring/intro/entities/User.java
+++ S03/3pg-spring-intro-s03-data-access/src/main/java/com/tpg/spring/intro/entities/User.java
@@ -1,10 +1,5 @@
 package com.tpg.spring.intro.entities;
 
-import org.apache.commons.lang3.builder.EqualsBuilder;
-import org.apache.commons.lang3.builder.HashCodeBuilder;
-import org.apache.commons.lang3.builder.ToStringBuilder;
-import org.apache.commons.lang3.builder.ToStringStyle;
-
 import java.io.Serializable;
 
 /**
@@ -62,20 +57,5 @@ public class User implements Serializable {
 
     public void setAge(Integer age) {
         this.age = age;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        return EqualsBuilder.reflectionEquals(this, o);
-    }
-
-    @Override
-    public int hashCode() {
-        return HashCodeBuilder.reflectionHashCode(this);
-    }
-
-    @Override
-    public String toString() {
-        return ToStringBuilder.reflectionToString(this, ToStringStyle.MULTI_LINE_STYLE);
     }
 }
