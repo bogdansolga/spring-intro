9ebd60978f9a1155daa429ae81abd03991fb6bdd
Bogdan Solga <bogdan.solga@gmail.com>
2014-10-23 15:31:09 +0300

    Removed the comments

--- S04/3pg-spring-intro-s04-service/src/main/java/com/tpg/spring/intro/service/UserService.java
+++ S04/3pg-spring-intro-s04-service/src/main/java/com/tpg/spring/intro/service/UserService.java
@@ -2,13 +2,6 @@ package com.tpg.spring.intro.service;
 
 import com.tpg.spring.intro.entities.User;
 
-/**
- * UserService class
- *
- * @author bogdan.solga
- *
- * Date: 27.06.2014, time: 09:33
- */
 public interface UserService {
     public User getJohnDoe();
 }
--- S04/3pg-spring-intro-s04-service/src/main/java/com/tpg/spring/intro/service/UserServiceImpl.java
+++ S04/3pg-spring-intro-s04-service/src/main/java/com/tpg/spring/intro/service/UserServiceImpl.java
@@ -3,13 +3,6 @@ package com.tpg.spring.intro.service;
 import com.tpg.spring.intro.dao.UserDAO;
 import com.tpg.spring.intro.entities.User;
 
-/**
- * UserServiceImpl class
- *
- * @author bogdan.solga
- *
- * Date: 27.06.2014, time: 09:36
- */
 public class UserServiceImpl implements UserService {
 
     private UserDAO usersDAO;
