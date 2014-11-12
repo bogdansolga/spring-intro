f64d32388b7022665894dab0db1b2489c569eda6
Bogdan Solga <bogdan.solga@gmail.com>
2014-11-12 11:12:15 +0200

    Removed the re-logout

--- S08/3pg-spring-intro-s08-webapp/src/main/webapp/js/controllers/MainController.js
+++ S08/3pg-spring-intro-s08-webapp/src/main/webapp/js/controllers/MainController.js
@@ -2,14 +2,13 @@
 
 var mainController = angular.module('mainController', []);
 
-mainController.controller('MainController', function($scope, $location, AuthServices) {
+mainController.controller('MainController', function($scope, $location) {
     $scope.$on('login', function(details) {
         $scope.authenticated = true;
         $location.path('/users');
     });
 
     $scope.$on('logout', function(details) {
-        AuthServices.logout($scope);
         $scope.authenticated = false;
         $location.path('/');
     });
