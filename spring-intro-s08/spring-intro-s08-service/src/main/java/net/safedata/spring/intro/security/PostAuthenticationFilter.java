812670961fbb9d680bc8b15bcf4caeb3bd960d1d
Bogdan Solga <bogdan.solga@dialogdata.de>
2015-04-02 08:06:56 +0300

    Enhanced the failed auth flow

--- spring-intro-s08/spring-intro-s08-service/src/main/java/net/safedata/spring/intro/security/PostAuthenticationFilter.java
+++ spring-intro-s08/spring-intro-s08-service/src/main/java/net/safedata/spring/intro/security/PostAuthenticationFilter.java
@@ -8,7 +8,9 @@ import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServletResponseWrapper;
 import java.io.IOException;
+import java.io.Writer;
 
 public class PostAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
@@ -21,6 +23,12 @@ public class PostAuthenticationFilter extends UsernamePasswordAuthenticationFilt
     @Override
     protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response,
                                               AuthenticationException failed) throws IOException, ServletException {
-        response.sendError(401, "Incorrect username or password");
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+
+        final HttpServletResponseWrapper responseWrapper = new HttpServletResponseWrapper(response);
+        final Writer out = responseWrapper.getWriter();
+
+        out.write("{\"message\": \"Incorrect username or password\"}");
+        out.flush();
     }
 }
