094a94290c113f616418b38e7286f3c127559e8e
Bogdan Solga <bogdan.solga@dialogdata.de>
2015-04-09 10:03:24 +0300

    Removed the unused imports

--- spring-intro-s12/spring-intro-s12-complete/src/main/java/net/safedata/spring/intro/config/DataSourceConfig.java
+++ spring-intro-s12/spring-intro-s12-complete/src/main/java/net/safedata/spring/intro/config/DataSourceConfig.java
@@ -7,13 +7,9 @@ import org.springframework.context.EnvironmentAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.orm.hibernate4.HibernateTransactionManager;
 import org.springframework.orm.hibernate4.LocalSessionFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-
-import javax.sql.DataSource;
 
 @Configuration
 @EnableTransactionManagement
