8a56dbb9aa6c5095f69b9dd00ffdd6ac46e5fb75
Bogdan Solga <bogdan.solga@dialogdata.de>
2015-04-09 11:48:18 +0300

    Small cleanup

--- spring-intro-s12/spring-intro-s12-complete/src/main/java/net/safedata/spring/intro/config/WebConfig.java
+++ spring-intro-s12/spring-intro-s12-complete/src/main/java/net/safedata/spring/intro/config/WebConfig.java
@@ -2,8 +2,6 @@ package net.safedata.spring.intro.config;
 
 import net.safedata.spring.intro.RunProfile;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
-import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
 import org.springframework.boot.context.embedded.ServletContextInitializer;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
@@ -13,7 +11,7 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
 
 @Configuration
-public class WebConfig implements ServletContextInitializer {
+public class WebConfig implements ServletContextInitializer {mall 
 
     @Autowired
     private Environment environment;
